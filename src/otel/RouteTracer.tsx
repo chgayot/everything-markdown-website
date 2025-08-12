726be2e07fc9997cee10524c4961ac9936e45944
chgayot <ch.gayot@gmail.com>
2025-08-13 00:07:43 +0200

    updating Otel tracer name

--- src/otel/RouteTracer.tsx
+++ src/otel/RouteTracer.tsx
@@ -6,7 +6,7 @@ export default function RouteTracer() {
   const location = useLocation();
 
   useEffect(() => {
-    const tracer = trace.getTracer("chgayot-web");
+    const tracer = trace.getTracer("website-blog");
     const span = tracer.startSpan("route_change", {
       attributes: {
         path: location.pathname,
